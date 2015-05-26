6dfdcafad9702cbda04ef12b7150a51d95a8c047
follower <vitfollower@gmail.com>
2015-05-26 21:23:23 +0300

    front end refactoring

--- frontend/modules/component.js
+++ frontend/modules/component.js
@@ -5,55 +5,51 @@ vf.registerModule('Component', {
 	dom: false,
 	templateOptions: {},
 
-	//
-
-	beforeActivate: function(params) {
+	beforeActivate: function (params) {
 	},
 
-	activate: function(params) {
+	activate: function (params) {
 		this.params = params;
 		this.beforeActivate(this.params);
 
-		if (this.autoRender != false || !params) {
-			if (this.dom) {
-				this.load();
-				this.renderWidgets();
-			} else {
-				this.loadTemplate();
-			}
+		if (this.dom) {
+			this.load();
+			this.renderComponents();
+		} else {
+			this.loadTemplate();
 		}
 	},
 
 	loadTemplate: function () {
-		vf.utils.loadTemplate(this.template, function(template) {
+		vf.utils.loadTemplate(this.template, function (template) {
 			this.dom = template;
 			this.load(this.params);
-			this.renderWidgets();
+			this.renderComponents();
 		}.bind(this));
 	},
 
-	load: function() {
+	load: function () {
 		this.loadComponents();
 		this.beforeRender();
 		this.render();
 		this.afterRender();
 	},
 
-	setTemplateOptions: function(obj) {
+	setTemplateOptions: function (obj) {
 		this.templateOptions = obj;
 		return this;
 	},
 
-	beforeRender: function(params) {
+	beforeRender: function (params) {
 	},
 
-	render: function() {
+	render: function () {
 		var container = vf.dom.find1(this.container);
 
 		if (container) {
 			//$(this.container).fadeOut(0);
 			container.innerHTML = vf.utils.render(this.dom, this.templateOptions);
-		//	$(this.container).fadeIn(500);
+			//	$(this.container).fadeIn(500);
 		} else {
 			throw 'Container error';
 		}
@@ -61,10 +57,10 @@ vf.registerModule('Component', {
 		this.registerDOMHandlers();
 	},
 
-	afterRender: function() {
+	afterRender: function () {
 	},
 
-	loadComponents: function() {
+	loadComponents: function () {
 		this.inlineComponents = {};
 
 		for (var alias in this.components) {
@@ -83,15 +79,15 @@ vf.registerModule('Component', {
 		}
 	},
 
-	getComponent: function(alias) {
+	getComponent: function (alias) {
 		return vf._registry.components[alias];
 	},
 
-	getInlineComponent: function(alias) {
+	getInlineComponent: function (alias) {
 		return this.inlineComponents[alias];
 	},
 
-	renderWidgets: function() {
+	renderComponents: function () {
 		for (var w in this.inlineComponents) {
 			var widget = this.inlineComponents[w];
 
@@ -101,7 +97,7 @@ vf.registerModule('Component', {
 		}
 	},
 
-	registerDOMHandlers: function() {
+	registerDOMHandlers: function () {
 		if (!!this.domHandlers) {
 			for (var i in this.domHandlers) {
 				var handler = this.domHandlers[i],
@@ -118,12 +114,11 @@ vf.registerModule('Component', {
 		}
 	},
 
-	find1: function(query) {
+	find1: function (query) {
 		return vf.dom.find1(this.container + ' ' + query);
 	},
 
-	find: function(query) {
+	find: function (query) {
 		return vf.dom.find(this.container + ' ' + query);
 	}
-
 });
---
+++ frontend/modules/model.js
@@ -0,0 +1,14 @@
+vf.registerModule('Model', {
+
+	data: {},
+
+	_api: function() {
+		return vf.module('Api');
+	},
+
+	get: function() {
+	},
+
+	save: function() {
+	}
+});
